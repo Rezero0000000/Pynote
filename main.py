4b822ed75087067e6ae07669dcd0ec7db470fa08
Rekun <hikarisakata91@gmail.com>
2023-06-22 20:22:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,30 @@ logoAscii = '''
  '''
 print(logoAscii)
 
-print("\n  +-------------------------+----------+")
-print("  | No |     Categories     |   Note   |")
-print("  |----|--------------------|----------|")
-print("  | 1  |   Cyber Security   |   256    |")
+def createTable (data, header):
+    # get Table Width
+    table_width = [max(len(str(item))for item in column)for column in zip(*data)]
+
+    #Create Header 
+    header = "+".join("-" * (width+2) for width in table_width)
+    table = []
+    table.append(header)
+    table.append(header)
+    return table
+    
+
+data = [
+    ["1", "Programming", 254],
+    ["2", "Cyber Security", 43],
+    ["3", "English", 140],
+]
+header = ['No', 'Categories', 'Notes']
+
+table = createTable(data, header)
+print(table)
+    
+
+#print("\n  +-------------------------+----------+")
+#print("  | No | Categories         | Note     |")
+#print("  |----|--------------------|----------|")
+#print("  | 1  | Cyber Security     | 256      |")
