9ab28b72e43e7380f00edd317ad8ba8f9ac83438
Rekun <hikarisakata91@gmail.com>
2023-07-05 14:32:49 +0700

    Delete useless file

--- note.py
+++
@@ -1,9 +0,0 @@
-
-options = """
-    [1]. Create Category
-    [2]. Select Category <No> 
-    [3]. Update Category <No>
-    [4]. Delete Category <No>
-    [5]. Search <Keyword>
-    [c]. Exit
-"""
